68fd5e264259d6b0873515b31b62ced819c7a125
live-HOCOK <zaordy@gmail.com>
2020-03-31 01:12:36 +0300

    update project
    * шары пуляются
    * шары отскакивают
    * шары умирают

---
+++ first_game/Assets/Scripts/Arrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public GameObject bullet;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.01f));
+        startPosition.z = 0f;
+        transform.position = startPosition;
+
+    }
+
+    void Update()
+    {
+        Aiming();
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+
+    private void Aiming()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 moveDirection = mousePos - (Vector2)transform.position;
+
+        if (moveDirection != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(-moveDirection.x, moveDirection.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
---
+++ first_game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 0.3f;
+
+    private Rigidbody2D rb;
+    private float right;
+    private float left;
+    private float top;
+    private float bottom;
+    private Vector2 force;
+    private bool onScreen = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        right = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f)).x;
+        left = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f)).x;
+        top = Camera.main.ViewportToWorldPoint(new Vector3(0f, 1f)).y;
+        bottom = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f)).y;
+        force = transform.up * speed;
+    }
+
+    void FixedUpdate()
+    {
+        MoveBullet();
+    }
+
+    void MoveBullet()
+    {
+        rb.AddForce(force);
+        if (transform.position.x >= right && onScreen)
+            ReverseX();
+        else if (transform.position.x <= left && onScreen)
+            ReverseX();
+        else if (transform.position.y >= top && onScreen)
+            ReverseY();
+        else if (transform.position.y <= bottom)
+            DestroyBullet();
+        else
+            onScreen = true;
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void ReverseX()
+    {
+        onScreen = false;
+        force.x = -force.x;
+        rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+    }
+
+    void ReverseY()
+    {
+        onScreen = false;
+        force.y = -force.y;
+        rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+    }
+}
